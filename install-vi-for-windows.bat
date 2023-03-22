95af4efefe9f7aa75054c2f2bb7865ca3061dbcf
sammtcbn <sammtcbn@gmail.com>
2023-03-22 15:41:24 +0800

    vim bump version to 9.0.1423

--- install-vi-for-windows.bat
+++ install-vi-for-windows.bat
@@ -3,17 +3,17 @@ set currdir=%~dp0
 IF "%currdir:~-1%"=="\" SET currdir=%currdir:~0,-1%
 cd /d "%currdir%"
 
-set ver=9.0.1418
-set fn=gvim_%ver%_x64.exe
+set ver=9.0.1423
+set fn=gvim_%ver%_x64.zip
 set url=https://github.com/vim/vim-win32-installer/releases/download/v%ver%/%fn%
 
 set installedpath=C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
 
 curl -k --fail --output %fn% -L %url%
 
-"C:\Program Files\7-Zip\7z" x %fn% -ogvim
+powershell -command "Expand-Archive -Force %fn% gvim"
 
-copy /y gvim\$0\vim.exe "%installedpath%\vi.exe"
+copy /y gvim\vim\vim90\vim.exe "%installedpath%\vi.exe"
 copy /y vimrc "%installedpath%\.vimrc"
 
 :END
